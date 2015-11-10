3b3abefb0462150d9c2e5fdff80e6533ca2f5410
Jesse Shawl <jesse@jshawl.com>
2015-11-10 13:31:13 -0500

    comment crud workin

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
+  default_scope { order('updated_at DESC') } # sorts by updated at in reverse orderr
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
+  default_scope { order('updated_at DESC') } # sorts by updated at in reverse orderr
   has_many :comments
 end
