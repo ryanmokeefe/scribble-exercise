220881a9c9c8592c50f359d60b8f0910eebe18c7
Jesse Shawl <jesse@jshawl.com>
2015-11-10 13:01:21 -0500

    post CRUD complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,28 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post)
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
